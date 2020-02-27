f2d35a12a47e3c4a1db10dc982280e89f5e50f3c
khoa <policekhoa13101998@gmail.com>
2020-02-27 18:38:28 +0700

    Get all users / Get user by id

--- index.js
+++ index.js
@@ -20,9 +20,11 @@ app.use(express.json());
 
 //Import routes
 const authRoute = require("./routes/auth");
+const userRoute = require("./routes/user");
 
 //Apply route
-app.use("/api/user", authRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/user", userRoute);
 
 app.listen(3000, () => {
   console.log("Server is running");
--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,23 @@
 const router = require("express").Router();
 const User = require("../models/User");
-const jwt = require("jsonwebtoken");
 const verifyToken = require("../middlewares/verifyToken");
+
+// Get all user
+router.get("/", verifyToken, async (req, res) => {
+  users = await User.find();
+  res.send(users);
+});
+
+// Get user by id
+router.get("/:id", verifyToken, async (req, res) => {
+  try {
+    user = await User.findById(req.params.id);
+    if (user == null) {
+      res.status(404).send({ error: "User not found" });
+    } else res.send(user);
+  } catch (error) {
+    res.status(404).send({ error: "User not found" });
+  }
+});
+
+module.exports = router;
